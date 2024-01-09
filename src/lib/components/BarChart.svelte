a189c01fc9c0d8f2adb1c08b187a1cd9745008c3
Anders Retzner <anders.retzner@gmail.com>
2024-01-09 07:51:20 +0100

    show days with strava activity as orange and add activity sport type to tooltip

--- src/lib/components/BarChart.svelte
+++ src/lib/components/BarChart.svelte
@@ -2,7 +2,7 @@
     import { scaleLinear, scaleTime } from "d3-scale";
     import { max, extent } from "d3-array";
     import { utcParse } from "d3-time-format";
-    import { ProgressBar } from '@skeletonlabs/skeleton';
+    import { fade } from "svelte/transition";
 
     import BarChartAxisY from "$lib/components/BarChartAxisY.svelte";
     import BarChartAxisX from "$lib/components/BarChartAxisX.svelte";
@@ -10,8 +10,11 @@
 
     export let width;
     export let data;
+    let dailySteps = data[0];
+    let sportTypes = data[1];
 
-    let hoverData;
+    let hoverSteps;
+    let hoverSportTypes;
     let focused;
 
     let margins = {
@@ -26,31 +29,46 @@
     $: innerWidth = width - margins.left - margins.right;
 
     let parseTime = utcParse("%Y-%m-%dT%H:%M:%S");
-    let extentTime = extent(data, (d) => parseTime(d.ts));
+    let extentTime = extent(dailySteps, (d) => parseTime(d.ts));
+
+    let activityDates = sportTypes.map((d) => d.ts);
 
     $: xScale = scaleTime()
         .domain(extentTime)
         .range([0, innerWidth - 10]);
     let yScale = scaleLinear()
-        .domain([0, max(data.map(d => d.steps))])
+        .domain([0, max(dailySteps.map(d => d.steps))])
         .range([innerHeight, 0]);
-
 </script>
 
-<div class="chart-container" role="img" on:mouseleave={() => { hoverData = null; focused = false; }} on:mouseenter={() => { focused = true; }}>
+<div class="chart-container" role="img" on:mouseleave={() => { hoverSteps = null; focused = false; hoverSportTypes = null; }} on:mouseenter={() => { focused = true; }}>
     <svg class="fill-current stroke-current text-current" {width} {height}>
         <g class="inner-chart" transform="translate({margins.left} {margins.top})">
-            {#each data as d}
-                <rect
+            {#each dailySteps as d}
+                {#if activityDates.includes(d.ts)}
+                <rect transition:fade={{ duration: 300 }}
                     role="img"
                     x={xScale(parseTime(d.ts))}
                     y={yScale(d.steps)}
                     height={innerHeight - yScale(d.steps)}
-                    width={innerWidth / data.length}
-                    opacity={hoverData ? (hoverData === d ? 1 : 0.75) : 1}
-                    on:mouseover={() => { hoverData = d; }}
-                    on:focus={() => { hoverData = d; }}
-                />
+                    width={innerWidth / dailySteps.length}
+                    opacity={hoverSteps ? (hoverSteps === d ? 1 : 0.75) : 1}
+                    fill={focused ? "#fc4c02" : "#fc4c02"}
+                    on:mouseover={() => { hoverSteps = d; hoverSportTypes = sportTypes.find((s) => s.ts === d.ts).sport_type ;}}
+                    on:focus={() => { hoverSteps = d; }}
+                />   
+                {:else}
+                    <rect
+                        role="img"
+                        x={xScale(parseTime(d.ts))}
+                        y={yScale(d.steps)}
+                        height={innerHeight - yScale(d.steps)}
+                        width={innerWidth / dailySteps.length}
+                        opacity={hoverSteps ? (hoverSteps === d ? 1 : 0.75) : 1}
+                        on:mouseover={() => { hoverSteps = d; hoverSportTypes = null; }}
+                        on:focus={() => { hoverSteps = d; }}
+                    />
+                {/if}
             {/each}
             <BarChartAxisX {xScale} height={innerHeight} />
             {#if focused}
@@ -58,8 +76,8 @@
             {/if}
         </g>
     </svg>
-    {#if hoverData}
-        <BarChartTooltip data={hoverData} width={innerWidth} {xScale} {yScale} />
+    {#if hoverSteps}
+        <BarChartTooltip steps={hoverSteps} sportTypes={hoverSportTypes} width={innerWidth} {xScale} {yScale} />
     {/if}
 </div>
 
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,7 +2,14 @@ import { PUBLIC_API_URL } from "$env/static/public";
 
 export async function load({ fetch }) {
     const fetchDailySteps = async () => {
-        const url = PUBLIC_API_URL + "/api/v1/steps";
+        let today = new Date();
+        let tomorrow = new Date();
+        tomorrow.setDate(today.getDate() + 1);
+        let ninetyDaysAgo = new Date();
+        ninetyDaysAgo.setDate(today.getDate() - 90);
+
+        const endpoint = "/api/v1/steps/range/?start=" + ninetyDaysAgo.toISOString().split("T")[0] + "&end=" + tomorrow.toISOString().split("T")[0];
+        const url = PUBLIC_API_URL + endpoint;
         const response = await fetch(url, { credentials: "include" });
         if (!response.ok) {
             throw new Error(response);
@@ -10,6 +17,24 @@ export async function load({ fetch }) {
         const data = await response.json();
         return data;
     }
+
+    const fetchActivityTypes = async () => {
+        let today = new Date();
+        let tomorrow = new Date();
+        tomorrow.setDate(today.getDate() + 1);
+        let ninetyDaysAgo = new Date();
+        ninetyDaysAgo.setDate(today.getDate() - 90);
+
+        const endpoint = "/api/v1/activities/type/range/?start=" + ninetyDaysAgo.toISOString().split("T")[0] + "&end=" + tomorrow.toISOString().split("T")[0];
+        const url = PUBLIC_API_URL + endpoint;
+        const response = await fetch(url, { credentials: "include" });
+        if (!response.ok) {
+            throw new Error(response);
+        }
+        const data = await response.json();
+        return data;
+    }
+
     const fetchLongestStreak = async () => {
         const threshold = 10000;
         const url = PUBLIC_API_URL + "/api/v1/steps/streak/?threshold=" + threshold;
@@ -23,7 +48,7 @@ export async function load({ fetch }) {
             
     return {
         streamed: {
-            dailySteps: fetchDailySteps(),
+            barChartData: Promise.all([fetchDailySteps(), fetchActivityTypes()]),
             longestStreak: fetchLongestStreak(),
         }
     }
